b89ef207ebfe70c0f6869b0ecaaf9e89e58f7767
a-hazza22 <7483088@ogrenci.bilecik.edu.tr>
2025-08-25 16:23:49 +0300

    اضفت دالة عند الضغط على الزر يتم اختيار صورة عشوائبة من المصفوفة

--- CapThat.js
+++ CapThat.js
@@ -19,4 +19,13 @@ const generatorButton = document.getElementById("generator-button");
 const randomMeme = document.getElementById("random-meme");
 // يربط المتغير randomCaption بعنصر التسمية التوضيحية العشوائية
 const randomCaption = document.getElementById("random-caption");
+// عند النقر على الزر، يتم اختيار صورة وتعليق عشوائيين من المصفوفات وتحديث المحتوى في الصفحة
+generatorButton.addEventListener("click", function() {
+  // اختيار فهرس عشوائي للصورة والتعليق
+  const randomMemeIndex = Math.floor(Math.random() * memeArray.length);
+  const randomCaptionIndex = Math.floor(Math.random() * captionArray.length);
+  // تحديث مصدر الصورة والتسمية التوضيحية
+  randomMeme.src = memeArray[randomMemeIndex];
+  randomCaption.innerText = captionArray[randomCaptionIndex];
+});
 
