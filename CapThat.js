73c97143bce3a2166c0e156555b86b7c740531e2
Abdullah Hazza <161160104+a-hazza22@users.noreply.github.com>
2025-08-25 15:55:04 +0300

    Add files via upload

---
+++ CapThat.js
@@ -0,0 +1,14 @@
+const memeArray = [
+  "./images/evil-kermit.png",
+  "./images/eddie-murphy-thinking.png",
+  "./images/futurama-fry.png",
+  "./images/confused-person.png"
+];
+
+const captionArray = [
+  "I am a level 60 on inferno mode!",
+  "Voted most likely to change the world.",
+  "Get this hair off my head!",
+  "Oooh that must have hurt."
+];
+
